7cff0f68a30bbeffdefb7b8eb6976991f13bcec5
Felix HH Yeung <felixhhyeung@hotmail.com>
2019-08-20 15:56:53 +0800

    done making a brand new dark mode button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,26 @@ import { RouterModule, Router, NavigationEnd } from '@angular/router';
 })
 export class AppComponent implements OnInit {
 	title = 'Moon';
-	darkModeCheckboxIsSelected: boolean = false;
+	darkModeIsSelected: boolean = false;
 	
 	ngOnInit() {
 		// dark mode
 		if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
-			this.darkModeCheckboxIsSelected = true;
+			this.darkModeIsSelected = true;
 		}
 	}
 	darkModeCheckboxChange() {
-		if(this.darkModeCheckboxIsSelected) {
+		if(this.darkModeIsSelected) {
+			document.body.classList.remove("themedLightOverride");
+			document.body.classList.add("themedDarkOverride");
+		} else {
+			document.body.classList.add("themedLightOverride");
+			document.body.classList.remove("themedDarkOverride");
+		}
+	}
+	darkModeFlipClick() {
+		this.darkModeIsSelected = !this.darkModeIsSelected;
+		if(this.darkModeIsSelected) {
 			document.body.classList.remove("themedLightOverride");
 			document.body.classList.add("themedDarkOverride");
 		} else {
