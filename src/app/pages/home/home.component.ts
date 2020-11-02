98093994493b1c0bdd507ccef07798245b06c358
Felix HH Yeung <felixhhyeung@hotmail.com>
2020-11-02 11:52:12 +0800

    in progress

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { version } from '../../../../package.json';
+import * as isOnline from 'is-online';
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
-	styleUrls: ['./home.component.scss']
+	styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
 	version: string = version;
+	isOnline: boolean = true;
 	constructor() { }
 
 	ngOnInit() {
+		setInterval(async() => {
+			this.isOnline = await isOnline();
+		}, 1000);
 	}
 
 }
